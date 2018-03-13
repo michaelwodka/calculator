47d5a98ebb1495e8d06a187b77384783fd505e64
mwodka <michaelwodka12@gmail.com>
2018-03-13 13:20:06 -0700

    update

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-function clicker (){
-    var buttons = document.getElementsByTagName("button");
-    var results = document.getElementById("results");
-    var calc = "";
-    for (var i=0;i<buttons.length;i++){
-        buttons[i].addEventListener("click", function(){
-        	calc = calc + this.innerHTML
+// function clicker (){
+//     var buttons = document.getElementsByTagName("button");
+//     var results = document.getElementById("results");
+//     var calc = "";
+//     for (var i=0;i<buttons.length;i++){
+//         buttons[i].addEventListener("click", function(){
+//         	calc = calc + this.innerHTML
 
-        	if (this.id == "AC") {
-        		calc = ""
-        		results.innerHTML = 0
-        	}
+//         	if (this.id == "AC") {
+//         		calc = ""
+//         		results.innerHTML = 0
+//         	}
 
-        	console.log(calc)
+//         	console.log(calc)
 
-        	if (this.id=="equal"){
-        		var numbers = calc.split(/\D/g)
-        		numbers = numbers.filter(Boolean)
-        		console.log(numbers)
-        		var operators = calc.split(/[0-9]/)
-        		operators = operators.filter(Boolean)
-        		console.log(operators)
+//         	if (this.id=="equal"){
+//         		var numbers = calc.split(/\D/g)
+//         		numbers = numbers.filter(Boolean)
+//         		console.log(numbers)
+//         		var operators = calc.split(/[0-9]/)
+//         		operators = operators.filter(Boolean)
+//         		console.log(operators)
 
-        		console.log(numbers[0])
+//         		console.log(numbers[0])
 
-        		var init = null
+//         		var init = null
 
-        		for (var i=0;i<numbers.length;i++){
-        			if (operators[i] == "x" && init == null) {
-        				var init = numbers[i]*numbers[i+1] 
-        			}
+//         		for (var i=0;i<numbers.length;i++){
+//         			if (operators[i] == "x" && init == null) {
+//         				var init = numbers[i]*numbers[i+1] 
+//         			}
 
-        			else if (operators[i] == "x" && init != null) {
-        				var init = init*numbers[i+1] 
-        			}
+//         			else if (operators[i] == "x" && init != null) {
+//         				var init = init*numbers[i+1] 
+//         			}
 
-        			else if (operators[i] == "/" && init == null) {
-        				var init = Number(numbers[i]) / Number(numbers[i+1]) 
-        			}
+//         			else if (operators[i] == "/" && init == null) {
+//         				var init = Number(numbers[i]) / Number(numbers[i+1]) 
+//         			}
 
-        			else if (operators[i] == "/" && init != null) {
-        				var init = Number(init) / Number(numbers[i+1]) 
-        			}
+//         			else if (operators[i] == "/" && init != null) {
+//         				var init = Number(init) / Number(numbers[i+1]) 
+//         			}
 
-         			else if (operators[i] == "+" && init == null) {
-        				var init = Number(numbers[i])+Number(numbers[i+1]) 
-        			}
+//          			else if (operators[i] == "+" && init == null) {
+//         				var init = Number(numbers[i])+Number(numbers[i+1]) 
+//         			}
 
-         			else if (operators[i] == "+" && init != null) {
-        				var init = Number(init)+Number(numbers[i+1]) 
-        			}
+//          			else if (operators[i] == "+" && init != null) {
+//         				var init = Number(init)+Number(numbers[i+1]) 
+//         			}
 
-         			else if (operators[i] == "-" && init == null) {
-        				var init = numbers[i]-numbers[i+1] 
-        			}
+//          			else if (operators[i] == "-" && init == null) {
+//         				var init = numbers[i]-numbers[i+1] 
+//         			}
 
-         			else if (operators[i] == "-" && init != null) {
-        				var init = init-numbers[i+1] 
-        			}
-        		}
+//          			else if (operators[i] == "-" && init != null) {
+//         				var init = init-numbers[i+1] 
+//         			}
+//         		}
 
-        		results.innerHTML = init
+//         		results.innerHTML = init
 
-        	}
-        })
-    }
-  }
+//         	}
+//         })
+//     }
+//   }
 
-  clicker()
+//   clicker()
